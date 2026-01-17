4ca71c36a71b9eec46b500caedb29777bea636c7
fa0311 <yuki@yuki0311.com>
2026-01-18 01:56:59 +0900

    format

--- src/hitomi/gallery.ts
+++ src/hitomi/gallery.ts
@@ -5,243 +5,270 @@ import { HentaiFatalError, HentaiHttpError, HentaiParseError } from "./error";
 const contentsDomain = "gold-usergeneratedcontent.net";
 
 type GGJsCode = {
-	case: string[];
-	b: string;
-	o: {
-		default: string;
-		match: string;
-	};
+  case: string[];
+  b: string;
+  o: {
+    default: string;
+    match: string;
+  };
 };
 const getGGJsCode = async (): Promise<GGJsCode> => {
-	const ggJsUrl = `https://ltn.${contentsDomain}/gg.js?_=${Date.now()}`;
-	const response = await fetch(ggJsUrl);
-	const ggJsText = await response.text();
+  const ggJsUrl = `https://ltn.${contentsDomain}/gg.js?_=${Date.now()}`;
+  const response = await fetch(ggJsUrl);
+  const ggJsText = await response.text();
 
-	const bMatch = /b:\s*'(\d+)\/'/.exec(ggJsText);
-	if (!bMatch) {
-		throw new HentaiParseError("Failed to extract 'b' value from gg.js");
-	}
+  const bMatch = /b:\s*'(\d+)\/'/.exec(ggJsText);
+  if (!bMatch) {
+    throw new HentaiParseError("Failed to extract 'b' value from gg.js");
+  }
 
-	const caseMatches = [...ggJsText.matchAll(/case\s+(\d+):/g)];
-	const caseArray = caseMatches.map((match) => match[1]);
+  const caseMatches = [...ggJsText.matchAll(/case\s+(\d+):/g)];
+  const caseArray = caseMatches.map((match) => match[1]);
 
-	const oMatches = [...ggJsText.matchAll(/o\s*=\s*(\d+);/g)];
-	if (oMatches.length < 2) {
-		throw new HentaiParseError("Failed to extract 'o' values from gg.js");
-	}
-	const defaultO = oMatches[0][1];
-	const matchO = oMatches[1][1];
+  const oMatches = [...ggJsText.matchAll(/o\s*=\s*(\d+);/g)];
+  if (oMatches.length < 2) {
+    throw new HentaiParseError("Failed to extract 'o' values from gg.js");
+  }
+  const defaultO = oMatches[0][1];
+  const matchO = oMatches[1][1];
 
-	return {
-		case: caseArray,
-		b: bMatch[1],
-		o: {
-			default: defaultO,
-			match: matchO,
-		},
-	};
+  return {
+    case: caseArray,
+    b: bMatch[1],
+    o: {
+      default: defaultO,
+      match: matchO,
+    },
+  };
 };
 
 const getWebpUrlFromHash = (hash: string, ggJs: GGJsCode): string => {
-	const directory1 = ggJs.b;
+  const directory1 = ggJs.b;
 
-	const dir2PartArray = /^.*(..)(.)$/.exec(hash);
-	if (!dir2PartArray) {
-		throw new HentaiFatalError(`Invalid hash format: ${hash}`);
-	}
-	const directory2 = parseInt(dir2PartArray[2] + dir2PartArray[1], 16).toString();
+  const dir2PartArray = /^.*(..)(.)$/.exec(hash);
+  if (!dir2PartArray) {
+    throw new HentaiFatalError(`Invalid hash format: ${hash}`);
+  }
+  const directory2 = parseInt(
+    dir2PartArray[2] + dir2PartArray[1],
+    16,
+  ).toString();
 
-	const subdomainMatch = ggJs.case.includes(directory2) ? ggJs.o.match : ggJs.o.default;
-	const subdomain = `w${parseInt(subdomainMatch, 10) + 1}`;
-	return `https://${subdomain}.${contentsDomain}/${directory1}/${directory2}/${hash}.webp`;
+  const subdomainMatch = ggJs.case.includes(directory2)
+    ? ggJs.o.match
+    : ggJs.o.default;
+  const subdomain = `w${parseInt(subdomainMatch, 10) + 1}`;
+  return `https://${subdomain}.${contentsDomain}/${directory1}/${directory2}/${hash}.webp`;
 };
 
 const getStreamUrlFromVideoFilename = (videoFilename: string): string => {
-	return `https://streaming.${contentsDomain}/videos/${videoFilename}`;
+  return `https://streaming.${contentsDomain}/videos/${videoFilename}`;
 };
 
 export const GalleryInfoFileSchema = z.object({
-	hash: z.string(),
-	height: z.number(),
-	width: z.number(),
-	name: z.string(),
-	hasavif: z.union([z.literal(0), z.literal(1)]).transform((val) => val === 1),
+  hash: z.string(),
+  height: z.number(),
+  width: z.number(),
+  name: z.string(),
+  hasavif: z.union([z.literal(0), z.literal(1)]).transform((val) => val === 1),
 });
 export type GalleryInfoFile = z.infer<typeof GalleryInfoFileSchema>;
 
 export const GalleryInfoSchema = z
-	.object({
-		language_localname: z.string(),
-		language_url: z.string(),
-		japanese_title: z.string().optional(),
-		id: z.transform((val) => Number(val)),
-		title: z.string(),
-		language: z.string(),
-		galleryurl: z.string(),
-		characters: z.array(z.object({ character: z.string(), url: z.string() })).default([]),
-		datepublished: z.string().optional(),
-		related: z.array(z.number()),
-		videofilename: z.string().optional(),
-		video: z.string().optional(),
-		artists: z.array(z.object({ artist: z.string(), url: z.string() })).default([]),
-		date: z.string().transform((val) => new Date(val)),
-		blocked: z.literal(0),
-		type: z.string(),
-		parodys: z.array(z.object({ parody: z.string(), url: z.string() })).default([]),
-		files: z.array(GalleryInfoFileSchema),
-		scene_indexes: z.array(z.number()),
-		tags: z.array(
-			z.object({
-				male: z.transform((val) => Boolean(val)),
-				url: z.string(),
-				tag: z.string(),
-				female: z.transform((val) => Boolean(val)),
-			}),
-		),
-		languages: z.array(
-			z.object({
-				galleryid: z.number(),
-				language_localname: z.string(),
-				url: z.string(),
-				name: z.string(),
-			}),
-		),
-		groups: z
-			.array(
-				z.object({
-					group: z.string(),
-					url: z.string(),
-				}),
-			)
-			.default([]),
-	})
-	.strict();
+  .object({
+    language_localname: z.string().optional(),
+    language_url: z.string(),
+    japanese_title: z.string().optional(),
+    id: z.transform((val) => Number(val)),
+    title: z.string(),
+    language: z.string(),
+    galleryurl: z.string(),
+    characters: z
+      .array(z.object({ character: z.string(), url: z.string() }))
+      .default([]),
+    datepublished: z.string().optional(),
+    related: z.array(z.number()),
+    videofilename: z.string().optional(),
+    video: z.string().optional(),
+    artists: z
+      .array(z.object({ artist: z.string(), url: z.string() }))
+      .default([]),
+    date: z.string().transform((val) => new Date(val)),
+    blocked: z.literal(0).optional(),
+    type: z.string(),
+    parodys: z
+      .array(z.object({ parody: z.string(), url: z.string() }))
+      .default([]),
+    files: z.array(GalleryInfoFileSchema),
+    scene_indexes: z.array(z.number()),
+    tags: z.array(
+      z.object({
+        male: z.transform((val) => Boolean(val)),
+        url: z.string(),
+        tag: z.string(),
+        female: z.transform((val) => Boolean(val)),
+      }),
+    ),
+    languages: z.array(
+      z.object({
+        galleryid: z.number(),
+        language_localname: z.string(),
+        url: z.string(),
+        name: z.string(),
+      }),
+    ),
+    groups: z
+      .array(
+        z.object({
+          group: z.string(),
+          url: z.string(),
+        }),
+      )
+      .default([]),
+  })
+  .strict();
 
 export type GalleryInfo = z.infer<typeof GalleryInfoSchema>;
 
 type GetGalleriesParam = {
-	galleryId: number;
-	headers: Record<string, string>;
+  galleryId: number;
+  headers: Record<string, string>;
 };
-const getGalleries = async ({ galleryId, headers }: GetGalleriesParam): Promise<GalleryInfo> => {
-	const galleriesUrl = `https://ltn.${contentsDomain}/galleries/${galleryId}.js`;
-	const response = await fetch(galleriesUrl, {
-		headers: {
-			...headers,
-			accept: "*/*",
-		},
-	});
-	if (!response.ok) {
-		throw new HentaiHttpError(`Failed to fetch galleries: ${galleriesUrl} - ${response.status} ${response.statusText}`);
-	}
-	const galleriesJsText = await response.text();
+const getGalleries = async ({
+  galleryId,
+  headers,
+}: GetGalleriesParam): Promise<GalleryInfo> => {
+  const galleriesUrl = `https://ltn.${contentsDomain}/galleries/${galleryId}.js`;
+  const response = await fetch(galleriesUrl, {
+    headers: {
+      ...headers,
+      accept: "*/*",
+    },
+  });
+  if (!response.ok) {
+    throw new HentaiHttpError(
+      `Failed to fetch galleries: ${galleriesUrl} - ${response.status} ${response.statusText}`,
+    );
+  }
+  const galleriesJsText = await response.text();
 
-	const jsonText = galleriesJsText
-		.replace(/^var galleryinfo = /, "")
-		.trim()
-		.replace(/;$/, "");
-	const prepared = z.preprocess(
-		(v) =>
-			Object.fromEntries(
-				Object.entries(v as Record<string, unknown>).map(([k, val]) => [k, val === null ? undefined : val]),
-			),
-		GalleryInfoSchema,
-	);
-	await fs.promises.writeFile("./src/hitomi/a.json", jsonText);
-	const galleries = prepared.parse(JSON.parse(jsonText));
-	return galleries;
+  const jsonText = galleriesJsText
+    .replace(/^var galleryinfo = /, "")
+    .trim()
+    .replace(/;$/, "");
+  const prepared = z.preprocess(
+    (v) =>
+      Object.fromEntries(
+        Object.entries(v as Record<string, unknown>).map(([k, val]) => [
+          k,
+          val === null ? undefined : val,
+        ]),
+      ),
+    GalleryInfoSchema,
+  );
+  await fs.promises.writeFile("./src/hitomi/a.json", jsonText);
+  const galleries = prepared.parse(JSON.parse(jsonText));
+  return galleries;
 };
 
 type DownloadImages = {
-	fileHashs: string[];
-	ggJs: GGJsCode;
-	headers: Record<string, string>;
+  fileHashs: string[];
+  ggJs: GGJsCode;
+  headers: Record<string, string>;
 };
 const downloadImages = async ({ fileHashs, ggJs, headers }: DownloadImages) => {
-	const webpUrls = fileHashs.map((fileHash) => {
-		const webpUrl = getWebpUrlFromHash(fileHash, ggJs);
-		return webpUrl;
-	});
+  const webpUrls = fileHashs.map((fileHash) => {
+    const webpUrl = getWebpUrlFromHash(fileHash, ggJs);
+    return webpUrl;
+  });
 
-	const list = webpUrls.map((webpUrl) => {
-		const callback = async () => {
-			const response = await fetch(webpUrl, {
-				headers: {
-					...headers,
-					accept: "image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8",
-				},
-			});
-			return response;
-		};
-		return callback;
-	});
+  const list = webpUrls.map((webpUrl) => {
+    const callback = async () => {
+      const response = await fetch(webpUrl, {
+        headers: {
+          ...headers,
+          accept:
+            "image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8",
+        },
+      });
+      return response;
+    };
+    return callback;
+  });
 
-	return list;
+  return list;
 };
 
 type DownloadHitomiParam = {
-	videofilename: string;
-	headers: Record<string, string>;
+  videofilename: string;
+  headers: Record<string, string>;
 };
 
-const downloadVideo = async ({ videofilename, headers }: DownloadHitomiParam) => {
-	const streamFile = getStreamUrlFromVideoFilename(videofilename);
-	const callback = async () => {
-		const response = await fetch(streamFile, {
-			headers: {
-				...headers,
-				accept: "image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8",
-				range: "bytes=0-",
-			},
-		});
-		return response;
-	};
+const downloadVideo = async ({
+  videofilename,
+  headers,
+}: DownloadHitomiParam) => {
+  const streamFile = getStreamUrlFromVideoFilename(videofilename);
+  const callback = async () => {
+    const response = await fetch(streamFile, {
+      headers: {
+        ...headers,
+        accept:
+          "image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8",
+        range: "bytes=0-",
+      },
+    });
+    return response;
+  };
 
-	return callback;
+  return callback;
 };
 
 type DownloadHitomiGalleriesParam = {
-	galleryId: number;
-	additionalHeaders?: Record<string, string>;
+  galleryId: number;
+  additionalHeaders?: Record<string, string>;
 };
 
 export type DownloadFileInfo =
-	| { type: "image"; file: GalleryInfoFile; callback: () => Promise<Response> }
-	| {
-			type: "video";
-			file: { name: string };
-			callback: () => Promise<Response>;
-	  };
+  | { type: "image"; file: GalleryInfoFile; callback: () => Promise<Response> }
+  | {
+      type: "video";
+      file: { name: string };
+      callback: () => Promise<Response>;
+    };
 
-export const downloadHitomiGalleries = async ({ galleryId, additionalHeaders = {} }: DownloadHitomiGalleriesParam) => {
-	const headers = {
-		...additionalHeaders,
-		"accept-language": "ja-JP,ja;q=0.9",
-		"cache-control": "no-cache",
-		pragma: "no-cache",
-		referer: `https://hitomi.la/reader/${galleryId}.html`,
-	};
-	const ggJs = await getGGJsCode();
-	const galleries = await getGalleries({ galleryId, headers });
+export const downloadHitomiGalleries = async ({
+  galleryId,
+  additionalHeaders = {},
+}: DownloadHitomiGalleriesParam) => {
+  const headers = {
+    ...additionalHeaders,
+    "accept-language": "ja-JP,ja;q=0.9",
+    "cache-control": "no-cache",
+    pragma: "no-cache",
+    referer: `https://hitomi.la/reader/${galleryId}.html`,
+  };
+  const ggJs = await getGGJsCode();
+  const galleries = await getGalleries({ galleryId, headers });
 
-	const list = [] as DownloadFileInfo[];
+  const list = [] as DownloadFileInfo[];
 
-	const fileHashs = galleries.files.map((e) => e.hash);
-	const imageList = await downloadImages({ fileHashs, ggJs, headers });
-	for (const [index, file] of galleries.files.entries()) {
-		list.push({ type: "image", file, callback: imageList[index] });
-	}
+  const fileHashs = galleries.files.map((e) => e.hash);
+  const imageList = await downloadImages({ fileHashs, ggJs, headers });
+  for (const [index, file] of galleries.files.entries()) {
+    list.push({ type: "image", file, callback: imageList[index] });
+  }
 
-	if (galleries.videofilename) {
-		const video = await downloadVideo({
-			videofilename: galleries.videofilename,
-			headers,
-		});
-		list.push({
-			type: "video",
-			file: { name: galleries.videofilename },
-			callback: video,
-		});
-	}
-	return [galleries, list] as const;
+  if (galleries.videofilename) {
+    const video = await downloadVideo({
+      videofilename: galleries.videofilename,
+      headers,
+    });
+    list.push({
+      type: "video",
+      file: { name: galleries.videofilename },
+      callback: video,
+    });
+  }
+  return [galleries, list] as const;
 };
